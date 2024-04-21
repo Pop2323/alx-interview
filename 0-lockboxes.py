137b1ba5c90788ec4167f8d087cf36a336579399
Pop2323 <omar4500455@gmail.com>
2024-04-22 01:15:49 +0200

    lockboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    for i in range(1, len(boxes)):
+        check = False
+        for j in range(len(boxes)):
+            if i in boxes[j] and i != j:
+                check = True
+                break
+        if not check:
+            return False
+    return True
