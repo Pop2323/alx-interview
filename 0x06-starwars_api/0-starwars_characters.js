d6bcb9ab370a19644c3bc134f33862d091ea8899
Pop2323 <omar4500455@gmail.com>
2024-05-23 11:03:30 -0700

    starwarApi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 const req = (arr, i) => {
   if (i === arr.length) return;
