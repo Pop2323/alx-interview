914e374aee35ee096383932c395ab20b7881bb57
Pop2323 <omar4500455@gmail.com>
2024-06-06 15:00:11 -0700

    makingChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+"""
+making_change
+"""
+
+
 def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins
+    needed to meet a given amount total.
+    """
 
     if total <= 0:
         return 0
