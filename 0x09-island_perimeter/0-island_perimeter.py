dc0ab85ec411984634eea9dc892459ef7ab7439d
Pop2323 <omar4500455@gmail.com>
2024-06-10 05:11:38 -0700

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+def island_perimeter(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    num_land_cells = 0
+    num_shared_edges = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                num_land_cells += 1
+                
+                if col > 0 and grid[row][col - 1] == 1:
+                    num_shared_edges += 1
+                
+                if row > 0 and grid[row - 1][col] == 1:
+                    num_shared_edges += 1
+
+    perimeter = num_land_cells * 4 - num_shared_edges * 2
+    return perimeter
